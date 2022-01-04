f4cd08c8526f3819b7e57c85481eac897e859b18
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-01-04 16:37:19 +0100

    Add dot at the end of the docstring following pep257
    Co-authored-by: Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>

--- pydocstringformatter/formatting/base.py
+++ pydocstringformatter/formatting/base.py
@@ -3,19 +3,19 @@ import tokenize
 
 
 class Formatter:
-    """Base class for docstring formatter"""
+    """Base class for docstring formatter."""
 
     @abc.abstractmethod
     def treat_token(self, tokeninfo: tokenize.TokenInfo) -> tokenize.TokenInfo:
-        """Return a modified token"""
+        """Return a modified token."""
 
 
 class StringFormatter(Formatter):
-    """Base class for formatter that only modifies the string content"""
+    """Base class for formatter that only modifies the string content."""
 
     @abc.abstractmethod
     def _treat_string(self, tokeninfo: tokenize.TokenInfo) -> str:
-        """Return a modified string"""
+        """Return a modified string."""
 
     def treat_token(self, tokeninfo: tokenize.TokenInfo) -> tokenize.TokenInfo:
         return tokenize.TokenInfo(
