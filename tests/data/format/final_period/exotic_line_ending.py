a9f94bf13b08fe33f784ed7f0a0fc39e2a8549e2
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-02-10 22:10:01 +0100

    Add ':' and ';' to allowed end of sentence punctuation (#52)

---
+++ tests/data/format/final_period/exotic_line_ending.py
@@ -0,0 +1,11 @@
+def function():
+    """Check the name of first argument, expect:
+
+      *'self' for a regular method
+      *'cls' for a class method or a metaclass regular method"""
+
+def function():
+    """This has peculiar line ending;
+
+    but it should not matter.
+                """
