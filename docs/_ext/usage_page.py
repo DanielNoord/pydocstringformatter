db7a9167cd46668e796aa258ed4072d50945b984
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-02-18 21:33:24 +0100

    Add Usage page to docs

---
+++ docs/_ext/usage_page.py
@@ -0,0 +1,36 @@
+import subprocess
+
+from sphinx import application
+
+
+def write_usage_page() -> None:
+    """Write docs/usage.rst."""
+
+    # Get the help message
+    with subprocess.Popen(
+        ["pydocstringformatter", "-h"], stdout=subprocess.PIPE
+    ) as proc:
+        assert proc.stdout
+        out = proc.stdout.readlines()
+
+    # Add correct indentation for the code block
+    help_message = "    ".join(i.decode("utf-8") for i in out)
+    # Remove indentation from completely empty lines
+    help_message = help_message.replace("    \n", "\n")
+
+    with open("usage.rst", mode="w", encoding="utf-8") as file:
+        file.write(
+            f"""Usage
+=====
+
+Current usage of ``pydocstringformatter``:
+
+.. code-block:: shell
+
+    {help_message}"""
+        )
+
+
+def setup(_: application.Sphinx) -> None:
+    """Required function to register the Sphinx extension."""
+    write_usage_page()
