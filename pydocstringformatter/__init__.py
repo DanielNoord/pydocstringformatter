1ff481654e1e00ac4bfd5e745a36d344a6969946
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-03-15 08:12:51 +0100

    Bump to ``0.5.2``

--- pydocstringformatter/__init__.py
+++ pydocstringformatter/__init__.py
@@ -8,7 +8,7 @@ from pydocstringformatter.utils.exceptions import (
     PydocstringFormatterError,
 )
 
-__version__ = "0.6.0-dev"
+__version__ = "0.5.2"
 
 
 def run_docstring_formatter(argv: Union[List[str], None] = None) -> None:
