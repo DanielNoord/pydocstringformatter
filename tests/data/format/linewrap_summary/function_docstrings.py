316ab4ab25bee1c2015dea58e9efac2cec6f1e37
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-04-28 14:06:44 +0200

    Update linewrap_summary test output

--- tests/data/format/linewrap_summary/function_docstrings.py
+++ tests/data/format/linewrap_summary/function_docstrings.py
@@ -1,12 +1,12 @@
 def func():
-    """A very long summary line that needs to be wrapped. A very long summary line that needs to be wrapped.
+    """A very long summary line that needs to be wrapped, A very long summary line that needs to be wrapped.
 
     A description that is not too long.
     """
 
 
 def func():
-    """A very long multi-line summary line that needs to be wrapped. A very long multi-line summary line that needs to be wrapped.
+    """A very long multi-line summary line that needs to be wrapped, A very long multi-line summary line that needs to be wrapped.
     A very long summary line that needs to be wrapped.
 
     A description that is not too long.
@@ -14,7 +14,7 @@ def func():
 
 
 def func():
-    """A multi-line summary that can be on one line.
+    """A multi-line summary that can be on one line,
     But it has a new line so it isn't.
 
     A description that is not too long.
@@ -24,4 +24,4 @@ def func():
 # Since the ending quotes will be appended on the same line this
 # exceeds the max length.
 def func():
-    """A multi-line summary that can be on one line. Something that is just too longgg."""
+    """A multi-line summary that can be on one line, Something that is just too longgg."""
