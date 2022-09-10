1940d1ba50c37937f6199cb3da27dba6e2791520
J. Sebastian Paez <jspaezp@users.noreply.github.com>
2022-09-10 09:37:04 -0500

    fixed linting issue

--- pydocstringformatter/_formatting/_utils.py
+++ pydocstringformatter/_formatting/_utils.py
@@ -6,6 +6,6 @@ def is_rst_title(summary: str) -> bool:
     """Check if the second line of a summary is one recurring character."""
     # If second line is one recurring character we're dealing with a rst title
     last_line = summary.splitlines()[-1].lstrip()
-    if len(last_line) == 0:
+    if len(last_line) < 1:
         return False
     return last_line.count(last_line[0]) == len(last_line)
