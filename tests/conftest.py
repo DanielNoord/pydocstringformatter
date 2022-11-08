70b62ced5dd2f796e010b515f633882ba0463c9f
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-11-08 12:45:14 +0100

    Ignore py_use tmp_path instead of deprecated tmpdir

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,11 @@
 import pytest
-from py._path.local import LocalPath  # type: ignore[import]
+from pathlib import Path
 
 
 @pytest.fixture
-def test_file(tmpdir: LocalPath) -> str:
+def test_file(tmp_path: Path) -> str:
     """A test file to be used by tests."""
-    filename = tmpdir / "test.py"
+    filename = tmp_path / "test.py"
     with open(filename, "w", encoding="utf-8") as file:
         file.write('"""A multi-line\ndocstring."""')
     return str(filename)
