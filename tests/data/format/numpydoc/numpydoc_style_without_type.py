61d44a181975018152d4c328dcc1a77e1aece9bc
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2023-02-26 21:54:24 +0100

    Add test for numpy without a type

---
+++ tests/data/format/numpydoc/numpydoc_style_without_type.py
@@ -0,0 +1,13 @@
+def sincos(theta):
+    """Returns
+    ----
+    sin: float
+        the sine of theta
+    cos: float
+        the cosine of theta
+    Parameters
+    -----
+    theta:
+        the angle at which to calculate the sine and cosine.
+"""
+    return math.sin(theta), math.cos(theta)
